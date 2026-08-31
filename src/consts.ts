cf94dce95adde2aea8352ae72da27b7c6517b9b0
Brayan Espinoza <bjespinozah@gmail.com>
2026-08-31 14:52:57 -0500

    Update blog style

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Marmota Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Marmota';
+export const SITE_DESCRIPTION = 'I write words, climb mountains, and make things.';
